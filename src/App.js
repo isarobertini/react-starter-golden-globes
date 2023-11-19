5b2d9a483df3bb6efa8cb4743c21d7e9844ec54b
Isa Robertini <robertiniisa@gmail.com>
2023-11-19 17:53:32 +0100

    changed deploy errors

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { NominationItem } from 'nominationItem';
+import { NominationItem } from './nominationItem'; // Corrected the import path
 
 export const App = () => {
   const [nominations, setNominations] = useState([]);
-  const [showWinning, setShowWinning] = useState(true); // Initial state to show winning nominations
+  const [showWinning, setShowWinning] = useState(true);
 
   useEffect(() => {
-    // Fetch data from your Express.js API endpoint
     axios.get('https://project-express-api-30od.onrender.com/nominations/')
       .then((response) => {
-        // Filter nominations based on whether they won or not
         const filteredNominations = showWinning
           ? response.data.filter((nomination) => nomination.win)
           : response.data;
 
-        // Assuming nominations are ordered by date, you can use slice to get the latest 20
         setNominations(filteredNominations.slice(0, 20));
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
       });
-  }, [showWinning]); // Update nominations when the showWinning state changes
+  }, [showWinning]);
 
   const backgroundStyle = {
     backgroundImage: `url('https://images.unsplash.com/photo-1518676590629-3dcbd9c5a5c9?q=80&w=1728&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
--- src/nominationItem.jsx
+++ src/nominationItem.jsx
@@ -1,12 +1,13 @@
+import React from 'react';
+
 export const NominationItem = ({ nominee, film, category, yearFilm, yearAward }) => {
     return (
-        <li className='bg-white bg-opacity-75 rounded-3xl'>
-            <h1 className="mt-10 text-3xl font-bold underline text-center">{nominee}</h1>
+        <li className='bg-white bg-opacity-75 rounded-3xl p-4 mb-4'>
+            <h1 className="text-3xl font-bold underline text-center">{nominee}</h1>
             <h2 className="text-sm font-bold text-center">{film}</h2>
-            <h3>{category}</h3>
-            <h3>Created: {yearFilm}</h3>
-            <h3>Nominated: {yearAward}</h3>
+            <h3 className="text-center">{category}</h3>
+            <h3 className="text-center">Created: {yearFilm}</h3>
+            <h3 className="text-center">Nominated: {yearAward}</h3>
         </li>
     );
 };
-
